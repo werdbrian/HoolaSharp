7e78fb2d9b02ebc8b49d7d407d51c25f3a6bdee3
hyein6790 <hyein6790@gmail.com>
2015-10-25 02:07:12 +0900

    Q Delay Fixed

--- Hoola Riven/Riven.cs
+++ Hoola Riven/Riven.cs
@@ -463,7 +463,7 @@ namespace HoolaRiven
         {
             if (Utils.GameTimeTickCount - lastQ >= 3650 && QStack != 1 && !Player.IsRecalling() && KeepQ) saveq();
             if (!Q.IsReady(500) || QStack == 4) QStack = 1;
-            if (forceQ && Orbwalking.CanMove(5) && QTarget != null && QTarget.IsValidTarget())
+            if (forceQ && Orbwalking.CanMove(15) && QTarget != null && QTarget.IsValidTarget())
             {
                 if (Q.IsReady()) Q.Cast(QTarget.Position);
             }
