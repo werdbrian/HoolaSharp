86a04399dd8c06a8bf7cc0f37916f7271676c0f1
hyein6790 <hyein6790@gmail.com>
2015-10-26 00:46:49 +0900

    Many Option Added

--- Hoola Lucian/Program.cs
+++ Hoola Lucian/Program.cs
@@ -20,6 +20,19 @@ namespace HoolaLucian
         private static bool AAPassive;
         private static bool HEXQ { get { return Menu.Item("HEXQ").GetValue<bool>(); } }
         private static bool KillstealQ { get { return Menu.Item("KillstealQ").GetValue<bool>(); } }
+        private static bool CQ { get { return Menu.Item("CQ").GetValue<bool>(); } }
+        private static bool CW { get { return Menu.Item("CW").GetValue<bool>(); } }
+        private static bool CE { get { return Menu.Item("CE").GetValue<bool>(); } }
+        private static bool HQ { get { return Menu.Item("HQ").GetValue<bool>(); } }
+        private static bool HW { get { return Menu.Item("HW").GetValue<bool>(); } }
+        private static bool HE { get { return Menu.Item("HE").GetValue<bool>(); } }
+        private static bool JQ { get { return Menu.Item("JQ").GetValue<bool>(); } }
+        private static bool JW { get { return Menu.Item("JW").GetValue<bool>(); } }
+        private static bool JE { get { return Menu.Item("JE").GetValue<bool>(); } }
+        private static bool LHQ { get { return Menu.Item("LHQ").GetValue<bool>(); } }
+        private static bool LQ { get { return Menu.Item("LQ").GetValue<bool>(); } }
+        private static bool LW { get { return Menu.Item("LW").GetValue<bool>(); } }
+        private static bool LE { get { return Menu.Item("LE").GetValue<bool>(); } }
         static bool AutoQ { get { return Menu.Item("AutoQ").GetValue<KeyBind>().Active; } }
         private static int MinMana { get { return Menu.Item("MinMana").GetValue<Slider>().Value; } }
 
@@ -59,6 +72,11 @@ namespace HoolaLucian
             TargetSelector.AddToMenu(targetSelectorMenu);
             Menu.AddSubMenu(targetSelectorMenu);
 
+            var Combo = new Menu("Combo", "Combo");
+            Combo.AddItem(new MenuItem("CQ", "Use Q").SetValue(true));
+            Combo.AddItem(new MenuItem("CW", "Use W").SetValue(true));
+            Combo.AddItem(new MenuItem("CE", "Use E").SetValue(true));
+            Menu.AddSubMenu(Combo);
 
             var Misc = new Menu("Misc", "Misc");
             Misc.AddItem(new MenuItem("Nocolision", "Nocolision W").SetValue(true));
@@ -67,7 +85,23 @@ namespace HoolaLucian
 
             var Harass = new Menu("Harass", "Harass");
             Harass.AddItem(new MenuItem("HEXQ", "Use Extended Q").SetValue(true));
+            Harass.AddItem(new MenuItem("HQ", "Use Q").SetValue(true));
+            Harass.AddItem(new MenuItem("HW", "Use W").SetValue(true));
+            Harass.AddItem(new MenuItem("HE", "Use E").SetValue(true));
             Menu.AddSubMenu(Harass);
+
+            var LC = new Menu("LaneClear", "LaneClear");
+            LC.AddItem(new MenuItem("LHQ", "Use Extended Q For Harass").SetValue(true));
+            LC.AddItem(new MenuItem("LQ", "Use Q").SetValue(true));
+            LC.AddItem(new MenuItem("LW", "Use W").SetValue(true));
+            LC.AddItem(new MenuItem("LE", "Use E").SetValue(true));
+            Menu.AddSubMenu(LC);
+
+            var JC = new Menu("JungleClear", "JungleClear");
+            JC.AddItem(new MenuItem("JQ", "Use Q").SetValue(true));
+            JC.AddItem(new MenuItem("JW", "Use W").SetValue(true));
+            JC.AddItem(new MenuItem("JE", "Use E").SetValue(true));
+            Menu.AddSubMenu(JC);
 
             var Auto = new Menu("Auto", "Auto");
             Auto.AddItem(new MenuItem("AutoQ", "Auto Extended Q (Toggle)").SetValue(new KeyBind('T', KeyBindType.Toggle)));
@@ -98,6 +132,14 @@ namespace HoolaLucian
                     Utility.DelayAction.Add(5, () => OnDoCastDelayed(args));
                 }
             }
+            if (args.Target is Obj_AI_Minion)
+            {
+                var target = (Obj_AI_Base)args.Target;
+                if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear && target.IsValid)
+                {
+                    Utility.DelayAction.Add(5, () => OnDoCastDelayed(args));
+                }
+            }
         }
 
         static void killsteal()
@@ -121,15 +163,32 @@ namespace HoolaLucian
                 if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && target.IsValid)
                 {
                     if (ItemData.Youmuus_Ghostblade.GetItem().IsReady()) ItemData.Youmuus_Ghostblade.GetItem().Cast();
-                    if (E.IsReady() && !AAPassive) E.Cast(Game.CursorPos);
-                    if (Q.IsReady() && !E.IsReady() && !AAPassive) Q.Cast(target);
-                    if (!E.IsReady() && !Q.IsReady() && W.IsReady() && !AAPassive && Orbwalking.CanMove(10)) W.Cast(target.Position);
+                    if (E.IsReady() && !AAPassive && CE) E.Cast(Game.CursorPos);
+                    if (Q.IsReady() && (!E.IsReady() || (E.IsReady() && !CE)) && CQ && !AAPassive) Q.Cast(target);
+                    if ((!E.IsReady() || (E.IsReady() && !CE)) && (!Q.IsReady() || (Q.IsReady() && !CQ)) && CW && W.IsReady() && !AAPassive) W.Cast(target.Position);
                 }
                 if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed && target.IsValid)
                 {
-                    if (E.IsReady() && !AAPassive) E.Cast(Game.CursorPos);
-                    if (Q.IsReady() && !E.IsReady() && !AAPassive) Q.Cast(target);
-                    if (!E.IsReady() && !Q.IsReady() && W.IsReady() && !AAPassive) W.Cast(target.Position);
+                    if (E.IsReady() && !AAPassive && HE) E.Cast(Game.CursorPos);
+                    if (Q.IsReady() && (!E.IsReady() || (E.IsReady() && !HE)) && HQ && !AAPassive) Q.Cast(target);
+                    if ((!E.IsReady() || (E.IsReady() && !HE)) && (!Q.IsReady() || (Q.IsReady() && !HQ)) && HW && W.IsReady() && !AAPassive) W.Cast(target.Position);
+                }
+            }
+            if (args.Target is Obj_AI_Minion)
+            {
+                var Mobs = MinionManager.GetMinions(Orbwalking.GetRealAutoAttackRange(Player), MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
+                if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear && Mobs[0].IsValid)
+                {
+                    if (E.IsReady() && !AAPassive && JE) E.Cast(Game.CursorPos);
+                    if (Q.IsReady() && (!E.IsReady() || (E.IsReady() && !JE)) && JQ && !AAPassive) Q.Cast(Mobs[0]);
+                    if ((!E.IsReady() || (E.IsReady() && !JE)) && (!Q.IsReady() || (Q.IsReady() && !JQ)) && JW && W.IsReady() && !AAPassive) W.Cast(Mobs[0].Position);
+                }
+                var Minions = MinionManager.GetMinions(Orbwalking.GetRealAutoAttackRange(Player));
+                if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear && Minions[0].IsValid)
+                {
+                    if (E.IsReady() && !AAPassive && LE) E.Cast(Game.CursorPos);
+                    if (Q.IsReady() && (!E.IsReady() || (E.IsReady() && !LE)) && LQ && !AAPassive) Q.Cast(Minions[0]);
+                    if ((!E.IsReady() || (E.IsReady() && !LE)) && (!Q.IsReady() || (Q.IsReady() && !LQ)) && LW && W.IsReady() && !AAPassive) W.Cast(Minions[0].Position);
                 }
             }
         }
@@ -156,7 +215,28 @@ namespace HoolaLucian
                 }
             }
         }
+        static void LaneClear()
+        {
+            if (Q.IsReady() && LHQ)
+            {
+                var t1 = TargetSelector.GetTarget(Q1.Range, TargetSelector.DamageType.Physical);
+                if (t1.IsValidTarget(Q1.Range) && Player.Distance(t1.ServerPosition) > Q.Range + 100)
+                {
+                    var qpred = Q.GetPrediction(t1, true);
+                    var distance = Player.Distance(qpred.CastPosition);
+                    var minions = MinionManager.GetMinions(Player.ServerPosition, Q.Range, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.MaxHealth);
 
+                    foreach (var minion in minions.Where(minion => minion.IsValidTarget(Q.Range)))
+                    {
+                        if (qpred.CastPosition.Distance(Player.Position.Extend(minion.Position, distance)) < 25)
+                        {
+                            Q.Cast(minion);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
         static void AutoUseQ()
         {
             if (Q.IsReady() && AutoQ && Player.ManaPercent > MinMana)
@@ -184,6 +264,7 @@ namespace HoolaLucian
         {
             W.Collision = Menu.Item("Nocolision").GetValue<bool>();
             AutoUseQ();
+            LaneClear();
             killsteal();
             if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed) Harass();
         }
