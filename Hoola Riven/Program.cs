a6dae2d712b83d5d62b2d209b4dcbd8d134c497e
hyein6790 <hyein6790@gmail.com>
2015-10-25 17:14:34 +0900

    Not use Logic Fixed

--- Hoola Riven/Program.cs
+++ Hoola Riven/Program.cs
@@ -249,6 +249,9 @@ namespace HoolaRiven
             if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo) Combo();
             if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear) Jungleclear();
             if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed) Harass();
+            if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.FastHarass) FastHarass();
+            if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Burst) Burst();
+            if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Flee) Flee();
         }
 
         static void killsteal()
@@ -532,7 +535,7 @@ namespace HoolaRiven
         {
             if (Utils.GameTimeTickCount - lastQ >= 3650 && QStack != 1 && !Player.IsRecalling() && KeepQ) saveq();
             if (!Q.IsReady(500) || QStack == 4) QStack = 1;
-            if (forceQ && Orbwalking.CanMove(15) && QTarget != null && QTarget.IsValidTarget(E.Range + Player.BoundingRadius + 70) && (Orbwalker.ActiveMode != Orbwalking.OrbwalkingMode.None || Orbwalker.ActiveMode != Orbwalking.OrbwalkingMode.LastHit))
+            if (forceQ && Orbwalking.CanMove(1) && QTarget != null && QTarget.IsValidTarget(E.Range + Player.BoundingRadius + 70) && (Orbwalker.ActiveMode != Orbwalking.OrbwalkingMode.None || Orbwalker.ActiveMode != Orbwalking.OrbwalkingMode.LastHit))
             {
                 if (Q.IsReady()) Q.Cast(QTarget.Position);
             }
